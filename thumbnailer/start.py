989ba282153b5b09405eb50e435421a5c61c3a7c
paulproteus <paulproteus@committer.creativecommons.org>
2008-02-05 17:51:00 +0000

    latest from http://my.opera.com/area42/blog/generating-thumbnails-using-gimp

--- thumbnailer/start.py
+++ thumbnailer/start.py
@@ -1,42 +1,45 @@
-# Source: http://www.nabble.com/thumbnail-generation-for-nautilus-via-gimp-td14108887.html
+#!/usr/bin/python
 
 from gimpfu import *
 import os
 import os.path
 
 def python_thumbnailer(this_path):
-  image_formats = [
-    ".psd",
-    ".xcf",
-    ".rgb"
-  ]
-  num_files = len (os.listdir(this_path))
-  all_files = os.listdir(this_path)
- 
-  for this_file in range(num_files):
-    this_ext = ''
-    this_filepath = this_path+"/"+all_files[this_file]
-    if os.path.isfile(this_filepath):
-      this_ext = splitext(this_filepath)[1]
-   
-      if this_ext in image_formats:
-        pdb.gimp_file_load(this_filepath,this_filepath)
-        pdb.gimp_file_save_thumbnail(this_filepath,this_filepath)
-        pdb.gimp_image_delete(this_filepath)
-  pdb.gimp_quit(0)
+	image_formats = [ 
+		".psd",
+		".xcf",
+		".rgb", 
+		".dds"
+	]
+	img = ""
+	num_files = len (os.listdir(this_path))
+	all_files = os.listdir(this_path)
+	
+	for this_file in range(num_files):
+		this_ext = ''
+		this_filepath = this_path+"/"+all_files[this_file]
+		if os.path.isfile(this_filepath):
+			this_ext = os.path.splitext(this_filepath)[1]
+		
+			if this_ext in image_formats:
+				img = pdb.gimp_file_load(this_filepath,this_filepath)
+				pdb.gimp_file_save_thumbnail(img,this_filepath)
+				pdb.gimp_image_delete(img)
+	pdb.gimp_quit(0)
+	
 register(
-  "python-fu-thumbnailer",
-  "Generating thumbnails",
-  "Generating thumbnails for Nautilus via Gimp",
-  "Eckhard M. Jaeger",
-  "Eckhard M. Jaeger",
-  "2007",
-  "<Toolbox>/Xtns/Nautilus Thumbnailer...",
-  "",
-  [
-    (PF_STRING, "this_path", "Directory Path", ""),
-  ],
-  [],
-  python_thumbnailer)
+	"python-fu-thumbnailer",
+	"Generating thumbnails",
+	"Generating thumbnails for Nautilus via Gimp",
+	"Eckhard M. Jaeger",
+	"Eckhard M. Jaeger",
+	"2007",
+	"<Toolbox>/Xtns/Nautilus Thumbnailer...",
+	"",
+	[
+		(PF_STRING, "this_path", "Directory Path", ""),
+	],
+	[],
+	python_thumbnailer)
 
 main()
