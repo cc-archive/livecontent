289f3384bfae7ea0f2d983b3ad15df310f67d0f9
paulproteus <paulproteus@committer.creativecommons.org>
2008-02-06 01:42:12 +0000

    directory making

--- thumbnailer/genthumb.py
+++ thumbnailer/genthumb.py
@@ -8,7 +8,10 @@ def gen_thumb(thumbnail_storage_land,
     for (size, height, width) in ( ('normal', 128, 128),
                                    ('large',  256, 256) ):
         outpath = os.path.join(thumbnail_storage_land, size)
-        thumb = gtk.gdk.pixbuf_new_from_file_at_size(height, width)
+        if not os.path.exists(outpath):
+            os.makedirs(outpath, mode=0755)
+
+        thumb = gtk.gdk.pixbuf_new_from_file_at_size(path_to_image, height, width)
         for hashme in alleged_filenames:
             if not hashme.startswith('file://'):
                 hashme = 'file://' + hashme 
@@ -19,6 +22,7 @@ def gen_thumb(thumbnail_storage_land,
                 {'tEXt::Thumb::URI': hashme,
                  'tEXt::Thumb::MTime': str(os.stat(path_to_image)[8]),
                  'tEXt::Thumb::Size': str(os.stat(path_to_image)[6])})
+    return out_filename
 
 def main():
     # Note: This part is a little hard-codey.  Sorry about that.
